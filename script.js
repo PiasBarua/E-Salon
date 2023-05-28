05e7460de6ff12d4f02ceb9e580136edd166cb38
PiasBarua <piasbarua.bd@gmail.com>
2023-05-28 23:13:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+/*Filter Intraction */
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all") {
+
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+
+    } else {
+
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+
+    }
+  }
+}
+addEventOnElem(filterBtns, "click", filter);
+
+window.addEventListener("scroll", function () {
+            var header = document.querySelector("header");
+            header.classList.toggle("sticky", window.scrollY > 0);
+        })
