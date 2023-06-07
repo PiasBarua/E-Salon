6476d232c3e02c74f2a105eee3570049b21d3dca
PiasBarua <piasbarua.bd@gmail.com>
2023-06-07 21:51:48 +0600

    databaseconnect

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "e-salon";
+
+    $con = mysqli_connect($servername,$username,$password,$dbname);
+
+    if($con){
+        echo "Sucessfully connected";
+    }
+
+?>
